ad7bb569a14edac8c7d67dd3a09bf5525fc4a857
Mac <mac.kittipat@gmail.com>
2021-05-15 17:15:09 +0700

    Create Authorization code flow sample

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Authorization Code sample with Azure AD
 
+Azure AD Application name : AzSampleApp
+
 ## Installation
 
 ```bash
@@ -21,29 +23,7 @@ $ npm run start:dev
 $ npm run start:prod
 ```
 
-## Test
+http://localhost:3000/
 
-```bash
-# unit tests
-$ npm run test
-
-# e2e tests
-$ npm run test:e2e
-
-# test coverage
-$ npm run test:cov
-```
-
-## Support
-
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
-
-## Stay in touch
-
-- Author - [Kamil Myśliwiec](https://kamilmysliwiec.com)
-- Website - [https://nestjs.com](https://nestjs.com/)
-- Twitter - [@nestframework](https://twitter.com/nestframework)
-
-## License
-
-Nest is [MIT licensed](LICENSE).
+## Reference : 
+* https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,83 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpService,
+  Logger,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { jwtVerify } from 'jose/jwt/verify';
+import { createRemoteJWKSet } from 'jose/jwks/remote';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+  private AZURE_AD_TENANT_ID: string;
+  private AZURE_AD_CLIENT_ID: string;
+  private AZURE_AD_SECRET: string;
+  private AZURE_AD_CALLBACK_URL: string;
+
+  constructor(
+    private appService: AppService,
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {
+    this.AZURE_AD_TENANT_ID = this.configService.get('AZURE_AD_TENANT_ID');
+    this.AZURE_AD_CLIENT_ID = this.configService.get('AZURE_AD_CLIENT_ID');
+    this.AZURE_AD_SECRET = this.configService.get('AZURE_AD_SECRET');
+    this.AZURE_AD_CALLBACK_URL = this.configService.get(
+      'AZURE_AD_CALLBACK_URL',
+    );
+  }
 
   @Get()
-  getHello(): string {
+  home(@Res() res) {
+    const authUrl =
+      `https://login.microsoftonline.com/${this.AZURE_AD_TENANT_ID}/oauth2/v2.0/authorize` +
+      `?client_id=${this.AZURE_AD_CLIENT_ID}` +
+      `&response_type=code` +
+      `&redirect_uri=${this.AZURE_AD_CALLBACK_URL}` +
+      `&scope=openid profile email`;
+
+    return res.redirect(authUrl);
+  }
+
+  @Get('/callback')
+  async callback(@Query() query) {
+    this.logger.log(`Auth code = ${query.code}`);
+    const tokenUrl = `https://login.microsoftonline.com/${this.AZURE_AD_TENANT_ID}/oauth2/v2.0/token`;
+
+    const params = new URLSearchParams();
+    params.append('client_id', this.AZURE_AD_CLIENT_ID);
+    params.append('grant_type', 'authorization_code');
+    params.append('code', query.code);
+    params.append('redirect_uri', this.AZURE_AD_CALLBACK_URL);
+    params.append('client_secret', this.AZURE_AD_SECRET);
+    params.append('scope', 'openid profile email');
+
+    const tokenRes = await this.httpService
+      .post(tokenUrl, params, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .toPromise();
+
+    const jwkSet = createRemoteJWKSet(
+      new URL(
+        `https://login.microsoftonline.com/${this.AZURE_AD_TENANT_ID}/discovery/v2.0/keys`,
+      ),
+    );
+    const { payload } = await jwtVerify(tokenRes.data.id_token, jwkSet);
+    this.logger.log(payload);
+
+    return tokenRes.data;
+  }
+
+  @Get('/helloworld')
+  helloWorld() {
     return this.appService.getHello();
   }
 }
